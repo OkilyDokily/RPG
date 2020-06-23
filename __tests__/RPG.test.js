68bf78fa120a270673bb8c85d2d84d8a43953cfb
me <me@me.me.com>
2020-06-23 14:47:23 -0700

    implement character creation spec

--- __tests__/RPG.test.js
+++ __tests__/RPG.test.js
@@ -1,1 +1,14 @@
-import {} from '../src/character.js'
+import {Character} from '../src/character.js'
+
+
+describe('Character', () => {
+  test('should correctly create a character object', () => {
+    const character = new Character();
+   
+    expect(character.health).toEqual(20);
+    expect(character.gold).toEqual(0);
+    expect(character.level).toEqual(1);
+    expect(character.XP).toEqual(0);
+    expect(character.weapon).toEqual(3);
+  });
+});
---
+++ src/character.js
@@ -0,0 +1,9 @@
+export class Character {
+  constructor(){
+    this.health = 20;
+    this.gold = 0;
+    this.XP = 0;
+    this.level = 1;
+    this.weapon = 3;
+  }
+}
